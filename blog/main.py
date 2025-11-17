e1cb6dc53ef40a5ab4368b2854bd518a408ad5f3
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-11-17 12:07:41 +0000

    Full CRUD operation circle

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, status, Response, HttpException
+from multiprocessing.sharedctypes import synchronized
+
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from .schemas import Blog
 from . import models
 from .database import engine, SessionLocal
@@ -32,12 +34,40 @@ def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    blog = (db.query(models.Blog).filter(models.Blog.id == id))
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"blog {id} to be deleted does not exist"
+        )
+    blog.delete(synchronize_session=False)
+
+    db.commit()
+
+    return blog
 
 @app.get('/blog/{id}', status_code=200)
-def show_specific_blog(id, response: Response, db: Session = Depends(get_db)):
+def show_specific_blog(id,  db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        raise HttpException(status_code= status.HTTP_404_NOT_FOUND, detail=f"blog with the id {id} is not available")
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"blog with the id {id} is not available")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'details': f'blog with the id {id} is not available'}
     return blog
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: Blog, db: Session = Depends(get_db)):
+    blog_query = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog_query.first():
+        raise HTTPException(
+            status_code =status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id}"
+        )
+    blog_query.update(request.dict())
+    db.commit()
+    return {'message': 'Updated Successfully!!!'}
